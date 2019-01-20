2dc09ca9e5b4ca2388d5b68bc0dcd5049e716bb3
BrunoMorii <brunomorii.1998@gmail.com>
2019-01-20 18:10:17 -0200

    Terminado Kmedias com pastas de todos resultados

--- kmedias.py
+++ kmedias.py
@@ -6,22 +6,22 @@ import pprint
 dados = [] #dsera matriz contendo informacao da leitura
 
 # Os três arquivos estão aqui, basta escolher qual
-leitura = "datasets\c2ds1-2sp.txt"
+#leitura = "datasets\c2ds1-2sp.txt"
 #leitura = "datasets\c2ds3-2g.txt"
-#leitura = "datasets\monkey.txt"
+leitura = "datasets\monkey.txt"
 
-f = open(leitura, "r");
+a = open(leitura, "r");
 
 #lendo linha a linha e colocando nos dados
 menorValor = 999999
 maiorValor = 0
-f.readline()
-for dado in f:
+a.readline()
+for dado in a:
     linha = dado.split('\t')
     dados.append([ str(linha[0]), float(linha[1]), float(linha[2]) ])
     menorValor = min(float(linha[1]), float(linha[2]), menorValor)
     maiorValor = max(float(linha[1]), float(linha[2]), maiorValor)
-f.close()
+a.close()
 
 
 #iniciando recebimento de informações
@@ -36,7 +36,7 @@ vetorCentroids = []
 #entre menor e maior valor nos dados
 for i in range(0, int(nCluster)):
     vetorCentroids.append([ random.randint(int(menorValor),int(maiorValor)), random.randint(int(menorValor),int(maiorValor))])
-
+    #vetorCentroids.append([ 5, 5]) #centroids inicias fixos
 #roda k media nInt vezes
 for i in range(0, int(nInt)):
     #inicializa clusters
@@ -49,7 +49,6 @@ for i in range(0, int(nInt)):
         #incializa comparadores
         distInd = -1
         distVal = 0
-
         #para cada centroid, verifica o melhor
         for k in vetorCentroids:
             dist = float( math.sqrt(pow( (j[1] - k[0]) , 2) + pow( (j[2] - k[1]) , 2) ) )
@@ -67,23 +66,60 @@ for i in range(0, int(nInt)):
     #calcula centroids novos
     vetorCentroids.clear()
     for j in range(0, int(nCluster)):
-        vetorCentroids.append([ 0.0,0.0])
+        vetorCentroids.append([0.0,0.0])
 
     for j in range(0, int(nCluster)):
         somaX = 0.0
         somaY = 0.0
+        divisor = 0
 
         for k in vetorCluster[j]:
             somaX += float(k[1])
             somaY += float(k[2])
+            divisor += 1
 
-        vetorCentroids[j][0] = somaX / len(vetorCentroids[j])
-        vetorCentroids[j][1] = somaY / len(vetorCentroids[j])
+        if(divisor != 0):
+            vetorCentroids[j][0] = somaX / divisor
+            vetorCentroids[j][1] = somaY / divisor
 
-    print("----------------")
-    print("Clusters:")
-    pprint.pprint(vetorCentroids)
+    #imprime para cada iteracao
+    """
+    print("-------- Iteração {0} --------".format(i))
+    print("======== Custers: ========")
+    for cluster in vetorCluster:
+        print("------------Cluster [ {0} ]: ".format(vetorCluster.index(cluster)))
+        for j in cluster:
+            print("   Nome: {0}. Ponto: {1}, {2}".format(j[0], j[1], j[2]))
+    print("======== Centroids: ========")
+    for j in range(0, int(nCluster)):
+        print("------------Centroid [ {0} ]: ".format(j))
+        for k in vetorCentroids:
+            print("   Centroid: {0}. Ponto: {1}, {2}".format(vetorCentroids.index(k), k[0], k[1]))
+    print("-------- ----------- --------")
+    """
 
-print("----------------")
-print("Clusters Finais:")
-pprint.pprint(vetorCentroids)
+#imprime valores finais
+"""
+print("-------- Valores Final --------")
+print("======== Custers: ========")
+for cluster in vetorCluster:
+    print("------------Cluster [ {0} ]: ".format(vetorCluster.index(cluster)))
+    for j in cluster:
+        print("   Nome: {0}. Ponto: {1}, {2}".format(j[0], j[1], j[2]))
+print("======== Centroids: ========")
+for j in range(0, int(nCluster)):
+    print("Centroid [ {0} ]: {1}, {2}".format(j, vetorCentroids[j][0], vetorCentroids[j][1]))
+"""
+
+#iniciando processo de escrita
+escrita = 'resultados\monkey\k' + str(nCluster) + '\monkeyKMedia.clu' #coloca na pasta de acordo com nCluster
+
+#para cada dado busca qual cluster esta e escreve
+a = open(escrita, "w")
+for i in dados:
+    for cluster in vetorCluster:
+        if i in cluster:
+            a.write( "{0}\t{1}\n".format(str(i[0]), vetorCluster.index(cluster)) )
+            break
+
+a.close()
